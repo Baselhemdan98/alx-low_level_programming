134f0c3316fa3346172833a0a5aed1c4e451005e
Baselhemdan98 <Baselhemdan1998@gmail.com>
2023-06-17 09:59:07 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry piont
+ *
+ * Description: print all digits
+ *
+ * Return: 0 (seccess)
+*/
+
+int main(void)
+{
+	int digit = 0;
+
+	while (0 <= 9)
+	{
+		putchar(digit + '0');
+		digit++;
+	}
+	putchar('\n');
+
+	return (0);
+}
+
